7e6c642a9951818f668d48076f145eb19f2ade40
parksangwoo <b01056721617@gmail.com>
2021-09-13 23:17:19 +0900

    📑

---
+++ src/main/java/com/study/querydsl/entity/Hello.java
@@ -0,0 +1,10 @@
+package com.study.querydsl.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Hello {
+}
--- src/test/java/com/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/study/querydsl/QuerydslApplicationTests.java
@@ -1,10 +1,18 @@
 package com.study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.study.querydsl.entity.Hello;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 @SpringBootTest
 class QuerydslApplicationTests {
+
+	@PersistenceContext
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
