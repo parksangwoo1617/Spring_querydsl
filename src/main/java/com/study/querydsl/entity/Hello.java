a7555eeda8fe1ea6d557e5b8f05cbf54b0190e7f
parksangwoo <b01056721617@gmail.com>
2021-09-14 20:17:40 +0900

    ⚡ :: 엔티티 등록

--- src/main/java/com/study/querydsl/entity/Hello.java
+++ src/main/java/com/study/querydsl/entity/Hello.java
@@ -4,7 +4,15 @@ package com.study.querydsl.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 @Getter
 @Setter
 public class Hello {
+
+    @Id @GeneratedValue
+    private Long id;
 }
