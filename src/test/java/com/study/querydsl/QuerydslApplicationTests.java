6683cf2b850e5c0d261d6c2c29982fe5a0004895
parksangwoo <b01056721617@gmail.com>
2021-09-15 23:20:43 +0900

    📑 :: test

--- src/test/java/com/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/study/querydsl/QuerydslApplicationTests.java
@@ -6,6 +6,7 @@ import com.study.querydsl.entity.QHello;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -35,5 +36,6 @@ class QuerydslApplicationTests {
 		assertThat(result).isEqualTo(hello);
 		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
+
 
 }
