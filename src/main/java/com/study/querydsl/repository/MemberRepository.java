ea7088c0b3cef1b91c65446dddd1cfdc1888506c
parksangwoo <b01056721617@gmail.com>
2021-09-22 00:21:49 +0900

    📑 :: 레포지토리 인터페이스

--- src/main/java/com/study/querydsl/repository/MemberRepository.java
+++ src/main/java/com/study/querydsl/repository/MemberRepository.java
@@ -1,2 +1,10 @@
-package com.study.querydsl.repository;public interface MemberRepository {
+package com.study.querydsl.repository;
+
+import com.study.querydsl.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+    List<Member> findByUsername(String username);
 }
